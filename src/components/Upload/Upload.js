df2a03a772a4015b0977e6ca5c35b1ed6b45b96a
yuvaraj m <95079980+Yuva8@users.noreply.github.com>
2022-12-06 13:40:16 +0530

    Update Upload.js

--- src/components/Upload/Upload.js
+++ src/components/Upload/Upload.js
@@ -1,32 +1,33 @@
-import React from 'react';
-import axios from 'axios';
-import { Progress } from 'reactstrap';
-import { ToastContainer, toast } from 'react-toastify';
-import { Navigate } from 'react-router-dom';
-import 'react-toastify/dist/ReactToastify.css';
-import './Upload.css';
-import Navbar from '../Navbar/Navbar';
+import React from "react";
+import axios from "axios";
+import { Progress } from "reactstrap";
+import { ToastContainer, toast } from "react-toastify";
+import { Navigate } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.css";
+import "./Upload.css";
+import Navbar from "../Navbar/Navbar";
 
 class Upload extends React.Component {
   state = {
     selectedVideos: null,
-    loaded: 0
-  }
+    loaded: 0,
+  };
 
   maxSelectFile(event) {
     let files = event.target.files;
     if (files.length > 1) {
-      toast.error('Maximum 1 file is allowed');
+      toast.error("Maximum 1 file is allowed");
       event.target.value = null;
       return false;
     } else {
-      let err = '';
+      let err = "";
       for (let i = 0; i < files.length; i++) {
-        if (files[i].size > 52428800) { // 50 MB
-          err += files[i].name + ', ';
+        if (files[i].size > 52428800) {
+          // 50 MB
+          err += files[i].name + ", ";
         }
       }
-      if (err !== '') {
+      if (err !== "") {
         // error caught
         event.target.value = null;
         toast.error(err + " is/are too large. Please select file size < 50Mb");
@@ -40,7 +41,7 @@ class Upload extends React.Component {
     if (this.maxSelectFile(event)) {
       this.setState({
         selectedVideos: files,
-        loaded: 0
+        loaded: 0,
       });
     }
   }
@@ -48,61 +49,91 @@ class Upload extends React.Component {
   fileUploadHandler(event) {
     const data = new FormData();
     for (let i = 0; i < this.state.selectedVideos.length; i++) {
-      data.append('file', this.state.selectedVideos[i]);
+      data.append("file", this.state.selectedVideos[i]);
     }
-    axios.post('https://videoappyuvi.herokuapp.com/api/upload', data,{
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer ' + JSON.parse(localStorage.getItem('userTokenTime')).token
-      }
-    },{
-      onUploadProgress: ProgressEvent => {
-        this.setState({
-          loaded: (ProgressEvent.loaded / ProgressEvent.total * 100)
-        });
-      }
-    }).then(res => {
-      toast.success('Upload Successful');
-    }).catch(err => {
-      toast.error(`Upload Fail with status: ${err.statusText}`);
-    });
+    axios
+      .post(
+        "https://yuvivideoapp-backend.onrender.com/api/upload",
+        data,
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization:
+              "Bearer " +
+              JSON.parse(localStorage.getItem("userTokenTime")).token,
+          },
+        },
+        {
+          onUploadProgress: (ProgressEvent) => {
+            this.setState({
+              loaded: (ProgressEvent.loaded / ProgressEvent.total) * 100,
+            });
+          },
+        }
+      )
+      .then((res) => {
+        toast.success("Upload Successful");
+      })
+      .catch((err) => {
+        toast.error(`Upload Fail with status: ${err.statusText}`);
+      });
   }
 
   render() {
-    if (!localStorage.getItem('userTokenTime')) return <Navigate to="/signIn" />
+    if (!localStorage.getItem("userTokenTime"))
+      return <Navigate to="/signIn" />;
     return (
       <>
-      <Navbar />
-       <div className="container mt-5">
+        <Navbar />
+        <div className="container mt-5">
           <div className="form-group">
             <ToastContainer />
           </div>
           <h4>Upload Video</h4>
           <hr className="my-4" />
 
-          <form method="post" name="videoUpload" action="/api/upload" id="#" encType="multipart/form-data">
+          <form
+            method="post"
+            name="videoUpload"
+            action="/api/upload"
+            id="#"
+            encType="multipart/form-data"
+          >
             <div className="form-group files">
               <label>Upload Your Videos Here</label>
-              <span style= {{paddingLeft:'550px'}}>Note:Videos can be uploaded in localhost port only</span>
+              <span style={{ paddingLeft: "550px" }}>
+                Note:Videos can be uploaded in localhost port only
+              </span>
               <input
                 type="file"
                 name="file"
                 className="form-control"
                 multiple="multiple"
                 accept="video/*"
-                onChange={this.fileChangeHandler.bind(this)} />
-              <Progress max="100" color="success" value={this.state.loaded} className="mt-4 mb-1">
-                {isNaN(Math.round(this.state.loaded, 2)) ? 0 : Math.round(this.state.loaded, 2)}%
+                onChange={this.fileChangeHandler.bind(this)}
+              />
+              <Progress
+                max="100"
+                color="success"
+                value={this.state.loaded}
+                className="mt-4 mb-1"
+              >
+                {isNaN(Math.round(this.state.loaded, 2))
+                  ? 0
+                  : Math.round(this.state.loaded, 2)}
+                %
               </Progress>
               <button
                 type="button"
                 className="btn btn-success btn-block"
-                onClick={this.fileUploadHandler.bind(this)}>Upload Video
+                onClick={this.fileUploadHandler.bind(this)}
+              >
+                Upload Video
               </button>
             </div>
           </form>
         </div>
-        </>
+      </>
     );
   }
 }
