162de945cc158031d56e012b33d2491e33c25b23
yuvaraj m <95079980+Yuva8@users.noreply.github.com>
2022-12-06 13:38:45 +0530

    Update SignUp.js

--- src/components/Form/SignUp/SignUp.js
+++ src/components/Form/SignUp/SignUp.js
@@ -1,82 +1,99 @@
-import React from 'react';
-import { Link, Navigate } from 'react-router-dom'
-import axios from 'axios';
+import React from "react";
+import { Link, Navigate } from "react-router-dom";
+import axios from "axios";
 
-import Form from '../form';
-import '../form.css';
+import Form from "../form";
+import "../form.css";
 
 class SignIn extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      firstName: '',
-      lastName: '',
-      email: '',
-      password: '',
-      navigate: localStorage.getItem('userTokenTime') ? true : false
-    }
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: "",
+      navigate: localStorage.getItem("userTokenTime") ? true : false,
+    };
 
     this.onSubmitHandler = this.onSubmitHandler.bind(this);
-    this.firstNameInputChangeHandler = this.firstNameInputChangeHandler.bind(this);
-    this.lastNameInputChangeHandler = this.lastNameInputChangeHandler.bind(this);
+    this.firstNameInputChangeHandler =
+      this.firstNameInputChangeHandler.bind(this);
+    this.lastNameInputChangeHandler =
+      this.lastNameInputChangeHandler.bind(this);
     this.emailInputChangeHandler = this.emailInputChangeHandler.bind(this);
-    this.passwordInputChangeHandler = this.passwordInputChangeHandler.bind(this);
+    this.passwordInputChangeHandler =
+      this.passwordInputChangeHandler.bind(this);
   }
 
-  onSubmitHandler (e) {
+  onSubmitHandler(e) {
     e.preventDefault();
-    if (!(this.state.firstName === '' || this.state.lastName === '' || this.state.email === '' || this.state.password === '')
-      && (/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(this.state.email))) {
-      axios.post('https://videoappyuvi.herokuapp.com/api/signUp', {
-        firstName: this.state.firstName,
-        lastName: this.state.lastName,
-        email: this.state.email,
-        password: this.state.password,
-        
-      }).then(res => {
-        this.setState({
-          navigate: true
+    if (
+      !(
+        this.state.firstName === "" ||
+        this.state.lastName === "" ||
+        this.state.email === "" ||
+        this.state.password === ""
+      ) &&
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+        this.state.email
+      )
+    ) {
+      axios
+        .post("https://yuvivideoapp-backend.onrender.com/api/signUp", {
+          firstName: this.state.firstName,
+          lastName: this.state.lastName,
+          email: this.state.email,
+          password: this.state.password,
+        })
+        .then((res) => {
+          this.setState({
+            navigate: true,
+          });
+        })
+        .catch((err) => {
+          console.log(err);
         });
-      }).catch(err => {
-        console.log(err);
-      });
     } else {
-      alert('Please enter valid details');
+      alert("Please enter valid details");
     }
   }
 
   firstNameInputChangeHandler(event) {
     this.setState({
-      firstName: event.target.value
+      firstName: event.target.value,
     });
   }
 
   lastNameInputChangeHandler(event) {
     this.setState({
-      lastName: event.target.value
+      lastName: event.target.value,
     });
   }
 
   emailInputChangeHandler(event) {
     this.setState({
-      email: event.target.value
+      email: event.target.value,
     });
   }
 
   passwordInputChangeHandler(event) {
     this.setState({
-      password: event.target.value
+      password: event.target.value,
     });
   }
 
   render() {
-    if (this.state.navigate) return <Navigate to='/' />
+    if (this.state.navigate) return <Navigate to="/" />;
     return (
       <Form onSubmit={this.onSubmitHandler.bind(this)}>
         <h3 className="text-center text-success">Register</h3>
         <div className="form-group">
-          <label htmlFor="first-name" className="text-success">First Name:</label><br />
+          <label htmlFor="first-name" className="text-success">
+            First Name:
+          </label>
+          <br />
           <input
             id="first-name"
             className="form-control"
@@ -84,10 +101,14 @@ class SignIn extends React.Component {
             name="firstName"
             placeholder="First Name"
             onChange={this.firstNameInputChangeHandler}
-            required />
+            required
+          />
         </div>
         <div className="form-group">
-          <label htmlFor="last-name" className="text-success">Last Name:</label><br />
+          <label htmlFor="last-name" className="text-success">
+            Last Name:
+          </label>
+          <br />
           <input
             id="last-name"
             className="form-control"
@@ -95,10 +116,14 @@ class SignIn extends React.Component {
             name="lastName"
             placeholder="Last Name"
             onChange={this.lastNameInputChangeHandler}
-            required />
+            required
+          />
         </div>
         <div className="form-group">
-          <label htmlFor="email" className="text-success">Email:</label><br />
+          <label htmlFor="email" className="text-success">
+            Email:
+          </label>
+          <br />
           <input
             id="email"
             className="form-control"
@@ -106,10 +131,14 @@ class SignIn extends React.Component {
             name="email"
             placeholder="example@domain.com"
             onChange={this.emailInputChangeHandler}
-            required />
+            required
+          />
         </div>
         <div className="form-group">
-          <label htmlFor="password" className="text-success">Password:</label><br />
+          <label htmlFor="password" className="text-success">
+            Password:
+          </label>
+          <br />
           <input
             id="password"
             className="form-control"
@@ -117,11 +146,20 @@ class SignIn extends React.Component {
             name="password"
             placeholder="********"
             onChange={this.passwordInputChangeHandler}
-            required />
+            required
+          />
         </div>
         <div className="d-flex justify-content-between align-items-end">
-          <input type="submit" name="submit" className="btn btn-success btn-md" value="Submit" style = {{marginTop:'10px'}} />
-          <Link to="/signIn" className="text-success">Login here</Link>
+          <input
+            type="submit"
+            name="submit"
+            className="btn btn-success btn-md"
+            value="Submit"
+            style={{ marginTop: "10px" }}
+          />
+          <Link to="/signIn" className="text-success">
+            Login here
+          </Link>
         </div>
       </Form>
     );
